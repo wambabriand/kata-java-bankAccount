e9709b7cbb7af71b64d68aa89ec3ce16ba71e176
wambabriand <briandwamba@yahoo.fr>
2022-07-16 16:20:59 +0200

    remove comment

--- bankAccount/src/main/java/com/bankAccount/entrypoints/BankController.java
+++ bankAccount/src/main/java/com/bankAccount/entrypoints/BankController.java
@@ -3,7 +3,6 @@ package com.bankAccount.entrypoints;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,13 +19,6 @@ import com.bankAccount.domain.usecase.OperationApi;
 @RestController
 public class BankController {
 
-/*
-	@Autowired
-	private AccountRepository accountRepository;
-	
-	@Autowired
-	private OperationRepository operationRepository;
-	*/
 	
 	private BankAccountUseCase bankAccountUseCase;
 	
